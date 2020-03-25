c6a91ede5954d96937f5c36e945e52f810c9aa87
zjcpx <zjcpx@hotmail.com>
2020-03-25 18:02:28 +0800

    完成

--- src/main/java/com/cfbe/function/cfbe.java
+++ src/main/java/com/cfbe/function/cfbe.java
@@ -23,6 +23,7 @@ import java.util.Scanner;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ObjectUtils.Null;
 
+import com.fasterxml.jackson.databind.ser.std.StaticListSerializerBase;
 import com.zjcpx.utils.ExcelUtil;
 
 /**  
@@ -30,7 +31,9 @@ import com.zjcpx.utils.ExcelUtil;
 * <p>Description: </p>  
 * @author Mike.Cai  
 * @date Mar 5, 2020  
-*/
+ * @author zjcpx
+ *
+ */
 public class cfbe {
 
 	/**  
@@ -47,76 +50,85 @@ public class cfbe {
 		String customsBillNo = "";
 		String invoiceNo = "";
 		String message = "";
-	
-		// 指定路径
+		List<File> fileList = new ArrayList<File>();
+
+		// 指定路径 
+		//  /Users/Mike/Desktop/CS/sj/01monthly summary.xls
+        //C:\Users\zjcpx\Desktop\CS2\sj\1111.xlsx
+		//d:\CS2\sj\1111.xlsx
+		//D:\CS\sj\01monthly summary副本.xls
+        //  /Users/Mike/Desktop/CS/sj/01monthly summary.xls
 		 Scanner sc = new Scanner(System.in); 
          System.out.println("请输入月度汇总文件的路径："); 
          String summaryDir = sc.nextLine();
-         //  /Users/Mike/Desktop/CS/sj/01monthly summary.xls
          
-         
-         //C:\Users\zjcpx\Desktop\CS\sj
-         //  /Users/Mike/Desktop/CS/sj/01monthly summary.xls
          System.out.println("请输入商检文件的路径："); 
          String sJDir = sc.nextLine();
+         sJDir = dealFilePath(sJDir);
          //处理输入路径最后的的“/”问题
-         String separatorChar = File.separator;
-         if (sJDir.endsWith(separatorChar)) {
-			sJDir = sJDir.substring(0,sJDir.lastIndexOf(separatorChar));
-		}
+        
          
-         //获取商检文件夹中的所有文件：files
-         List<File> files = getFiles(sJDir);
-         //查看检索到的文件 
-         for(int i = 0,len = files.size(); i < len; i++) {
-        	 System.out.println(files.get(i));
-         }
+         //获取商检文件夹中的所有文件:fileList
+         getFiles(sJDir,fileList);
+
          System.out.println("请输入结果存放的路径："); 
          String resultDir = sc.nextLine(); 
-     //  /Users/Mike/Desktop/CS/sj/
-         File f= new File(summaryDir) ;    // 声明File对象
-//         ArrayList<List<String>> resultList = new ArrayList<List<String>>();
+         resultDir = dealFilePath(resultDir);
+         
+         File f= new File(summaryDir) ; 
          InputStream in = null;
 		try {
 			in = new FileInputStream(f);
 			List<List<Object>> bankListByExcel = ExcelUtil.getBankListByExcel(in, summaryDir);
-			for(int i = 1,n = bankListByExcel.size(); i < n ; i++) {
-//				for(int j = 0,len=bankListByExcel.get(i).size();j<len; j++) {
-//					System.out.println("bankListByExcel("+i+","+j+")="+bankListByExcel.get(i).get(j));
-//				}		
+			for(int i = 0,n = bankListByExcel.size(); i < n ; i++) {
+//				
 				//IKEANo：IKEA流水号，如果读到的内容为空，则默认为上一次读到的内容
 				iKEANo = bankListByExcel.get(i).get(7) == null ? "":(String) bankListByExcel.get(i).get(7);
 				//customsBillNo: 报关单号
 				customsBillNo = (String) bankListByExcel.get(i).get(3);
 				//invoiceNo:发票号,如果读到的内容为空，则默认为上一次读到的内容
 				invoiceNo = bankListByExcel.get(i).get(8) == null ? "":(String) bankListByExcel.get(i).get(8);
+				
 				//根据指定的路径生成结果文件夹
-
 				File file = new File(resultDir+File.separator+"result"+File.separator+iKEANo+ File.separator);
+				
 				//根据Excel中的内容创建目录
 				if(!file.exists()) {
-					System.out.println(file.mkdirs());
+					file.mkdirs();
 				}
-				if (!copyFile((sJDir+File.separator+customsBillNo+".pdf"),file.toString()+File.separator+customsBillNo+".pdf")) {
-					message += customsBillNo+".PDF没有找到;\t";
+				//判断文件是否存在
+				File fileExist = isFileExist(customsBillNo+".pdf",fileList);
+				if(fileExist != null) {
+					copyFile(fileExist, file);
+					
+				}else {
+					message += "报关单号为："+customsBillNo+"没找到"; 
 				}
-//				if(!copyFile(fileList,(customsBillNo+".pdf"),file)) {
-////					System.out.println(customsBillNo+".PDF没有找到");
-//					message += customsBillNo+".PDF没有找到;\t";
-//				};
-//				if(!copyFile(fileList,(invoiceNo+".pdf"),file)) {
-////					System.out.println(invoiceNo+".PDF没有找到");
-//					message += invoiceNo+".PDF没有找到\t";
-//					
-//				};
-//				if(!copyFile(fileList,(invoiceNo+"-1.PDF"),file)) {
-////					System.out.println(invoiceNo+"-1.PDF没有找到\t");
-//					message += invoiceNo+"-1.PDF没有找到\t";
-//				};
-//				if(!copyFile(fileList,(invoiceNo+"-5.PDF"),file)) {
-////					System.out.println(invoiceNo+"-5.PDF没有找到");
-//					message += invoiceNo+"-5.PDF"+".PDF没有找到\r\n\r\n;";
-//				};
+				File fileExist1 = isFileExist(invoiceNo+".pdf",fileList);
+				if(fileExist1 != null) {
+					copyFile(fileExist1, file);
+				}else {
+					message += "发票号号为："+invoiceNo+".pdf没找到"; 
+				}
+				File fileExist2 = isFileExist(invoiceNo+".xls",fileList);
+				if(fileExist2 != null) {
+					copyFile(fileExist2, file);
+				}else {
+					message += "发票号号为："+invoiceNo+".xls没找到"; 
+				}
+				File fileExist3 = isFileExist(invoiceNo+"-1.pdf",fileList);
+				if(fileExist3 != null) {
+					copyFile(fileExist3, file);
+				}else {
+					message += "发票号号为："+invoiceNo+"-1.pdf没找到"; 
+				}
+				File fileExist4 = isFileExist(invoiceNo+"-5.pdf",fileList);
+				if(fileExist4 != null) {
+					copyFile(fileExist4, file);
+				}else {
+					message += "发票号号为："+invoiceNo+"-5.pdf没找到"; 
+				}
+
 			}
 			System.out.println(message);
 			
@@ -127,6 +139,14 @@ public class cfbe {
 		}
 		
 		
+	}
+	
+	public static String dealFilePath(String filePath) {
+		final String separatorChar = File.separator;
+		if (filePath.endsWith(separatorChar)) {
+			filePath = filePath.substring(0,filePath.lastIndexOf(separatorChar));
+		}
+		return filePath;
 	}
 	
 	/**
@@ -145,41 +165,7 @@ public class cfbe {
 		return fs;
 	}
 	
-	/**
-	 * 
-	 * <p>Title: copyFile</p>  
-	 * <p>Description: 根据参数file，遍历文件列表，匹配到，将文件拷贝到指定文件夹，返回TRUE，没匹配到返回false</p>  
-	* @author Mike.Cai  
-	* @date Mar 6, 2020  
-	 * @param files：文件列表
-	 * @param fileName：需要比对的文件名
-	 * @param dest：指定的文件夹
-	 */
 	
-//	public static Boolean copyFile(File[]files,String fileName,File dest) {
-//		//验证传参
-////		System.out.println("fileName is :"+fileName);
-////		System.out.println("dest is :"+dest);
-//		//遍历文件数组中的所有文件
-//		for(int i = 0,n = files.length; i < n; i++) {
-//			String file = files[i].toString();
-////			System.out.println(fileName);
-//			String fileNameNow = file.substring(file.lastIndexOf(File.separator)+1);
-////			System.out.println(fileNameNow);
-//			//
-//			if (fileName.equals(fileNameNow)) {
-//				try {
-//					FileUtils.copyFile(files[i], dest);
-//				}
-//				catch (IOException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//				return true;
-//				}
-//		}
-//		return false;
-//	}
 	/**
 	 * 
 	 * <p>Title: copyFile</p>  
@@ -189,24 +175,23 @@ public class cfbe {
 	 * @param oldPath
 	 * @param newPath
 	 */
-	public static Boolean copyFile(String oldPath, String newPath) { 
-		System.out.println("oldPath is " + oldPath+"; newPath is "+newPath);
+	public static Boolean copyFile(File file, File desPath) { 
 		try { 
 			int bytesum = 0; 
 			int byteread = 0; 
-			File oldfile = new File("/Users/Mike/Desktop/CS/sj/222920200000015086.pdf"); 
-			if (oldfile.exists()) { //文件存在时 
-				InputStream inStream = new FileInputStream(oldPath); //读入原文件 
-				FileOutputStream fs = new FileOutputStream(newPath); 
-				byte[] buffer = new byte[1444]; 
-				int length; 
-				while ( (byteread = inStream.read(buffer)) != -1) { 
-					bytesum += byteread; //字节数 文件大小 
-					System.out.println(bytesum); 
-					fs.write(buffer, 0, byteread); 
-				} 
-				inStream.close(); 
+			String fileName = file.getName();
+			String path = desPath.getAbsolutePath();
+			InputStream inStream = new FileInputStream(file); //读入原文件 
+			FileOutputStream fs = new FileOutputStream(path+File.separator+fileName); 
+			byte[] buffer = new byte[1444]; 
+			int length; 
+			while ( (byteread = inStream.read(buffer)) != -1) { 
+				bytesum += byteread; //字节数 文件大小 
+				
+				fs.write(buffer, 0, byteread); 
 			} 
+			inStream.close(); 
+
 			return true;
 		} 
 		catch (Exception e) { 
@@ -221,16 +206,34 @@ public class cfbe {
 	 * @param path
 	 * @return
 	 */
-	public static List<File> getFiles(String path) {
-		List<File> fileList = new ArrayList<File>();
+	public static void getFiles(String path,List<File> fileList) {
+		
 		File root = new File(path);
 		File[] files = root.listFiles();
 		for (File file : files) {
 			if(file.isDirectory()) {
-				getFiles(file.getAbsolutePath());
+				getFiles(file.getAbsolutePath(),fileList);
 			}
 			fileList.add(file);
 		}
-		return fileList;
+		
+	}
+	
+	/**
+	 * 
+	 * @param fileName
+	 * @param files
+	 * @return
+	 */
+	public static File isFileExist(String fileName, List<File> fileList) {
+		for(int i = 0,len = fileList.size(); i < len; i++) {
+			String fileString = fileList.get(i).getAbsolutePath();
+			String substring = fileString.substring(fileString.lastIndexOf(File.separator)+1);
+//			System.out.println(substring);
+			if(substring.equals(fileName)) {
+				return fileList.get(i);
+			}
+		}
+		return null;
 	}
 }
